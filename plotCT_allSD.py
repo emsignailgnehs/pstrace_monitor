63d79b066a89445a494d8ab0e88541da34923bfc
Sheng <jefferysaccount@gmail.com>
2023-03-10 13:03:10 -0800

    update calling cutoff to fix sal threshold issue

--- plotCT_allSD.py
+++ plotCT_allSD.py
@@ -49,7 +49,7 @@ print("Total Positive Data: "+str(sum(y)))
 print("Total Negative Data: "+str(len(y)-sum(y)))
 
 #%% Calculate
-cutoffStart = 7
+cutoffStart = 8
 cutoffEnd = 30
 normStart = cutoffStart
 normEnd = cutoffStart + 1
